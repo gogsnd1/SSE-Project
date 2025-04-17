d3f64848b3dfe796cb3ae63cc0b6ab9d25be0413
Reminoxel <151901089+Reminoxel@users.noreply.github.com>
2025-04-17 00:54:46 -0400

    Clicky Clicky

--- Assets/Scripts/LookScript.cs
+++ Assets/Scripts/LookScript.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class LookScript : MonoBehaviour
@@ -8,39 +7,45 @@ public class LookScript : MonoBehaviour
     [SerializeField] bool invert = false;
     [SerializeField] float sensitivity = 10.0f;
 
-
     [SerializeField] Camera playerCamera;
     [SerializeField] Transform playerBody;
+
+    [Header("Click Sound Settings")]
+    [SerializeField] AudioClip clickSound;         // Assign sound in Inspector
+    [SerializeField][Range(0f, 1f)] float clickVolume = 0.5f;  // Adjustable volume (0 to 1)
+
+    private AudioSource audioSource;
 
     private float rotationX = 0;
     private float rotationY = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // Setup AudioSource
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Mouse Look
         float mouseX = Input.GetAxis("Mouse X") * sensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
 
-
-        rotationY += mouseX; // Rotate around the Y-axis
-        if (invert)
-        {
-            rotationX -= -Input.GetAxis("Mouse Y") * sensitivity;
-        }
-        else
-        {
-            rotationX += -Input.GetAxis("Mouse Y") * sensitivity;
-        }
-        // rotationX += -Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY += mouseX;
+        rotationX += (invert ? 1 : -1) * mouseY;
         rotationX = Mathf.Clamp(rotationX, -90, 90);
+
         playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        transform.rotation *= Quaternion.Euler(0, mouseX, 0);
+
+        // Play click sound with volume
+        if (Input.GetMouseButtonDown(0) && clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound, clickVolume);
+        }
     }
 }
