8012830069195a34d13514d3f9cb585f14a4ca97
Reminoxel <151901089+Reminoxel@users.noreply.github.com>
2025-04-18 13:31:53 -0400

    QuickCommit

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour{
+    public AudioMixer audioMixer;
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
