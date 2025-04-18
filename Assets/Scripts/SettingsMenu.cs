0e24abaddfcd1bebc687b2670162ab4e3123b5e8
Reminoxel <151901089+Reminoxel@users.noreply.github.com>
2025-04-18 16:00:39 -0400

    MainMenuAlmostDone

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,30 +1,55 @@
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
+using TMPro;
 using System.Collections.Generic;
-public class SettingsMenu : MonoBehaviour{
 
+public class SettingsMenu : MonoBehaviour
+{
     Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+    public AudioMixer audioMixer;
 
-    public Dropdown resolutionDropdown;
     void Start()
     {
+        // Get all resolutions and prepare the dropdown
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
+
         List<string> options = new List<string>();
-        for (int i = 0; i < resolutions.Length; i++){
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            // Store index of the current screen resolution
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
+
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;  // auto-select current
+        resolutionDropdown.RefreshShownValue();             // update label
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);  // link dropdown
     }
-    public AudioMixer audioMixer;
-    public void setVolume(float volume)
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
 
-    public void SetFullscreen(bool isFullscreen){
+    public void SetFullscreen(bool isFullscreen)
+    {
         Screen.fullScreen = isFullscreen;
     }
 }
