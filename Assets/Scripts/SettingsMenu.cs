21ea16f3a8ac1ca82e72118f16f2fd7e2ffda305
Reminoxel <151901089+Reminoxel@users.noreply.github.com>
2025-04-18 15:32:24 -0400

    .

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
+using System.Collections.Generic;
 public class SettingsMenu : MonoBehaviour{
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++){
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+        resolutionDropdown.AddOptions(options);
+    }
     public AudioMixer audioMixer;
     public void setVolume(float volume)
     {
